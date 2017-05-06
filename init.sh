35c1fef7943755338d78892dd98651e99fd5d39f
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-05-06 17:18:39 +0900

    Add mkdir .narou to make narou.rb rocognize current directory is root directory for narou.

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 if [ ! -d "/opt/narou/.narou" ]; then
-  mkdir /opt/narou/.narousetting
-  unzip /opt/AozoraEpub3.zip -d /opt/narou/AozoraEpub3
+  unzip -q /opt/AozoraEpub3.zip -d /opt/narou/AozoraEpub3
   ln -s /opt/kindlegen/kindlegen /opt/AozoraEpub3
   narou init -p /opt/narou/AozoraEpub3
   narou setting device=kindle
+  # Do not make .narousetting nor .narou directory at the beginning
+  # since narou changes it's behavior of existance of them.
+  # However, we need global setting directory under /opt/narou,
+  # so moving it here.
+  mv $HOME/.narousetting /opt/narou/
   echo -e "---\nalready-server-boot: true" > /opt/narou/.narousetting/server_setting.yaml
 fi
 

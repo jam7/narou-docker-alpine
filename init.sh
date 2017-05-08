afc920c637830c697e223d8b92be1fcf155fed68
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-05-08 18:54:56 +0900

    Corrected `ln -s` path

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 if [ ! -d "/opt/narou/.narou" ]; then
   unzip -q /opt/AozoraEpub3.zip -d /opt/narou/AozoraEpub3
   cp /opt/AozoraEpub3.jar /opt/narou/AozoraEpub3
-  ln -s /opt/kindlegen/kindlegen /opt/AozoraEpub3
+  ln -s /opt/kindlegen/kindlegen /opt/narou/AozoraEpub3
   narou init -p /opt/narou/AozoraEpub3
   narou setting device=kindle
   # Do not make .narousetting nor .narou directory at the beginning

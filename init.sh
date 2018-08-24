725cf17efa425163011724715658eea9097883ee
Kazushi (Jam) Marukawa <jam@pobox.com>
2018-08-25 00:18:23 +0900

    Add -l option to first call of "narou init" for narou-3.2.0 and later.

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ if [ ! -d "/opt/narou/.narou" ]; then
   unzip -q /opt/AozoraEpub3.zip -d /opt/narou/AozoraEpub3
   cp /opt/AozoraEpub3.jar /opt/narou/AozoraEpub3
   ln -s /opt/kindlegen/kindlegen /opt/narou/AozoraEpub3
-  narou init -p /opt/narou/AozoraEpub3
+  narou init -p /opt/narou/AozoraEpub3 -l 1.6
   narou setting device=kindle
   # Do not make .narousetting nor .narou directory at the beginning
   # since narou changes it's behavior of existance of them.
